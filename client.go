c4f9a29e9fee64d1046460258506cb65c5b40055
Inessa Prokofyeva <inessa.t@icloud.com>
2017-10-28 16:41:55 -0700

    Sending messages from server to client and back

--- client.go
+++ client.go
@@ -7,17 +7,28 @@ import "os"
 
 func main() {
 
-  // connect to this socket
-  conn, _ := net.Dial("tcp", "10.112.3.37:8080")
-  for { 
-    // read in input from stdin
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Text to send: ")
-    text, _ := reader.ReadString('\n')
-    // send to socket
-    fmt.Fprintf(conn, text + "\n")
-    // listen for reply
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message from server: "+message)
-  }
+    // connect to this socket
+    conn, err := net.Dial("tcp", "127.0.0.1:8080")
+    if err != nil {
+        fmt.Println("dial error:", err)
+        return
+    }
+    fmt.Println("connected")
+    msg, _ := bufio.NewReader(conn).ReadString('\n')
+    fmt.Println("msg received")
+    fmt.Println(msg)
+    // fmt.Println(msg)
+    // fmt.Println("smth")
+    for { 
+        // read in input from stdin
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Print("Text to send: ")
+        text, _ := reader.ReadString('\n')
+        fmt.Printf("input is: %s", text)
+        // send to socket
+        fmt.Fprintf(conn, text)
+        // listen for reply
+        //message, _ := bufio.NewReader(conn).ReadString('\n')
+        //fmt.Print("Message from server: "+message)
+    }
 }
--- serv2.go
+++ serv2.go
@@ -4,12 +4,14 @@ import (
     "bufio"
     "fmt"
     "net"
+    "strings"
 )
 
 var allClients map[*Client]int
 
 type Client struct {
     // incoming chan string
+    nickname    string
     outgoing   chan string
     reader     *bufio.Reader
     writer     *bufio.Writer
@@ -24,7 +26,7 @@ func (client *Client) Read() {
             if client.connection != nil {
                 client.connection.outgoing <- line
             }
-            fmt.Println(line)
+            fmt.Print(client.nickname, ": ", line)
         } else {
             break
         }
@@ -55,13 +57,21 @@ func NewClient(connection net.Conn) *Client {
     writer := bufio.NewWriter(connection)
     reader := bufio.NewReader(connection)
 
+
+    //connection.Write([]byte("hello \n"))
+    writer.WriteString("Enter your nickname: \n")
+    writer.Flush()
+    fmt.Println("msg sent")
+    msg, _ := reader.ReadString('\n')
     client := &Client{
         // incoming: make(chan string),
+        nickname:   strings.TrimSpace(msg),
         outgoing: make(chan string),
         conn:     connection,
         reader:   reader,
         writer:   writer,
     }
+    fmt.Println("My name is " + client.nickname)
     client.Listen()
 
     return client
@@ -69,7 +79,8 @@ func NewClient(connection net.Conn) *Client {
 
 func main() {
     allClients = make(map[*Client]int)
-    listener, _ := net.Listen("tcp", "10.112.3.37:8080")
+    listener, _ := net.Listen("tcp", "127.0.0.1:8080")
+    fmt.Println("Server is running...")
     for {
         conn, err := listener.Accept()
         if err != nil {
