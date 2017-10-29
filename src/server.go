ee5180cb937bf5798777a3309b671ba3ed943825
Inessa Prokofyeva <inessa.t@icloud.com>
2017-10-29 16:30:26 -0700

    Sending proper msg to group

--- src/server.go
+++ src/server.go
@@ -18,12 +18,25 @@ type ConnectedClient struct {
 	hostname string
 	servername string
 	registered bool
+	channels map[string]string
 
 	msg_cnt int
 }
 
+type Channel struct {
+	name string
+	chan_clients map[string]*ConnectedClient
+}
+
 // key is a nickname
 var clients map[string]*ConnectedClient
+var channels map[string]*Channel
+
+
+func (client *ConnectedClient) respond_channel(msg string, channel_name string, person *ConnectedClient) {
+	//fmt.Printf(":%s!~%s@%s PRIVMSG %s %s\n", client.nickname, client.username, client.hostname, channel_name, msg)
+	fmt.Fprintf(person.conn, ":%s!~%s@%s PRIVMSG %s %s\n", client.nickname, client.username, client.hostname, channel_name, msg)
+}
 
 func (client *ConnectedClient) respond(msg string) {
 	fmt.Fprintf(client.conn, ":localhost %03d %s :%s\n", client.msg_cnt, client.nickname, msg)
@@ -85,12 +98,57 @@ func (client *ConnectedClient) privmsg_command(nickname string, text string) {
 		client.error(ERR_NOTREGISTERED)
 		return
 	}
-
 	receiver_client, ok := clients[nickname]
 	if ok {
 		receiver_client.respond(text)
 	} else {
+		fmt.Printf("here\n")
 		client.error(ERR_NOSUCHNICK, nickname)
+	}
+}
+
+func (client *ConnectedClient) msg_to_channel(channel_name string, text string) {
+	if !client.registered {
+		client.error(ERR_NOTREGISTERED)
+		return
+	}
+
+	receiver_channel, ok := channels[channel_name]
+	if ok {
+		for key:= range receiver_channel.chan_clients {
+			person := receiver_channel.chan_clients[key]
+			if person.username != client.username {
+				fmt.Printf("client:{%s}, person{%s}", client.nickname, person.nickname)
+				client.respond_channel(text, channel_name, person)
+				//client.privmsg_channel(person, text, channel_name)
+			}
+		}
+	} else {
+		fmt.Printf("here1\n")
+		client.error(ERR_NOSUCHNICK, channel_name)
+	}
+}
+
+func (client *ConnectedClient) join_channel(channel_name string) {
+	if !client.registered {
+		client.error(ERR_NOTREGISTERED)
+		return
+	}
+	fmt.Printf("{%s}\n", channel_name)
+	ch, ok := channels[channel_name]
+	if ok {
+		_, ok := ch.chan_clients[client.username]
+		if !ok {
+			ch.chan_clients[client.username] = client
+		}
+	} else {
+		channel := Channel {
+			name: channel_name,
+			chan_clients: make(map[string]*ConnectedClient),
+		}
+		channels[channel_name] = &channel
+		channel.chan_clients[client.username] = client
+		client.channels[channel_name] = channel_name
 	}
 }
 
@@ -124,8 +182,18 @@ func (client *ConnectedClient) handle_command(cmd string) {
 				msg += args[i]
 				msg += " "
 			}
-			fmt.Printf("msg: {%s}\n", msg)
-			client.privmsg_command(args[1], strings.TrimSuffix(msg, " "))
+			if args[1][0] == '#' {
+				client.msg_to_channel(args[1], strings.TrimSuffix(msg, " "))
+			} else {
+				client.privmsg_command(args[1], strings.TrimSuffix(msg, " "))
+			}
+		}
+	} else if fisrt_cmd == "JOIN" {
+		if len > 1 {
+			chan_lst := strings.Split(args[1], ",")
+			for i := range chan_lst {
+				client.join_channel(chan_lst[i])
+			}
 		}
 	} else {
 		if (client.registered) {
@@ -134,7 +202,7 @@ func (client *ConnectedClient) handle_command(cmd string) {
 	}
 
 	fmt.Println("$")
-	for key, _ := range clients {
+	for key, _ := range channels {
     	fmt.Printf("Key: %s\n", key)
 	}
 	fmt.Println("$")
@@ -144,6 +212,7 @@ func handle_conn(conn net.Conn) {
 	client := ConnectedClient {
 		conn: conn,
 		msg_cnt: 1,
+		channels: make(map[string]string),
 	}
 
 	reader := bufio.NewReader(conn)
@@ -166,6 +235,7 @@ func handle_conn(conn net.Conn) {
 
 func main() {
 	clients = make(map[string]*ConnectedClient)
+	channels = make(map[string]*Channel)
 
 	fmt.Println("Hello, 世界")
 	listener, _ := net.Listen("tcp", "127.0.0.1:6668")
